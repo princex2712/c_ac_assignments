6a6d20c0807d1e040f8bede14dc92948f9cd6a1f
Prince Patel <pp9882602@gmail.com>
2023-09-27 15:22:58 +0530

    commit

--- HomeWork/lwrupr.c
+++ HomeWork/lwrupr.c
@@ -8,9 +8,46 @@ int main(){
     scanf("%20s",arr);
     printf("\nEnter your choice:\n(1) for Lower\n(2) for Upper\n(3) for opposite you entered\n");
     scanf("%d",&c);
+    
+    //converting char to ascii value
+    for(int i=0;arr[i]!='\0';i++){
+        asciiArr[i]=(int)arr[i];
+    }
+
     switch(c){
         case 1:
-            
+            for(int i=0;asciiArr[i]!='\0';i++){
+                if(asciiArr[i]>=97){
+                    printf("%c",asciiArr[i]);
+                }else{
+                    asciiArr[i]=asciiArr[i]+32;
+                    printf("%c",asciiArr[i]);
+                }
+            }
             break;
+        case 2:
+            for(int i=0;asciiArr[i]!='\0';i++){
+                if(asciiArr[i]>=97){
+                    asciiArr[i]=asciiArr[i]-32;
+                    printf("%c",asciiArr[i]);
+                }else{
+                    printf("%c",asciiArr[i]);
+                }
+            }
+            break;
+        case 3:
+           for(int i=0;asciiArr[i]!='\0';i++){
+                if(asciiArr[i]>=97){
+                    asciiArr[i]=asciiArr[i]-32;
+                    printf("%c",asciiArr[i]);
+                }else{
+                    asciiArr[i]=asciiArr[i]+32;
+                    printf("%c",asciiArr[i]);
+                }
+            } 
+            break;
+        default:
+            printf("Invalid Operation");
     }
+    return 0;
 }       
