63cff3efc4c041bc4a9147c4eda0f84d048f5813
Prince Patel <pp9882602@gmail.com>
2023-10-11 18:46:28 +0530

    commit

--- Practice/question6.c
+++ Practice/question6.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(){
+    int n = 7;
     printf("\nPattern\n");
 
-    for(int i=1;i<=7;i++){
+    for(int i=1;i<=n;i++){
         if(i%2==0){
-            for(int j=1;j<=7;j++){
-                if(j==1 || j==7){
+            for(int j=1;j<=n;j++){
+                if(j==1 || j==n){
                     printf("* ");
                 }else if(j%2!=0 && j>i){
                     printf("* ");
@@ -14,24 +15,24 @@ int main(){
                 }
             }
         }else{
-            for(int j=1;j<=7;j++){
-               if(j%2==0 && j>1 && j <7 && i%2==0){
-                printf("  ");
-               }else if((i==3 || i==5) && j>i){
-                printf("  ");
-               }else{
-                printf("* ");
-               }
+            if(i%2!=0 && i>1 && i<n){
+                for(int j=1;j<=n;j++){
+                    if(j<=i){
+                        printf("* ");
+                    }else if(j==n || j%2!=0){
+                        printf("* ");
+                    }
+                    else{
+                        printf("  ");
+                    }
+                }
+            }else{
+                for(int j=1;j<=n;j++){
+                    printf("* ");
+                }
             }
+           
         }
         printf("\n");
     }
 }
-// * * * * * * * 
-// *   *   *   * 
-// * * * * * * * 
-// *       *   * 
-// * * * * * * * 
-// *           * 
-// * * * * * * * 
-
